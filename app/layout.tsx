67e94ef0f4006417895f3c40f1532df2454e76c6
bagoesrex <bagoesrex@gmail.com>
2026-05-08 00:55:21 +0700

    chore: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-jakarta-sans",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Gift Kuy - Kirim Hadiah Digital Instan",
+  description:
+    "Platform virtual gift untuk mengirim hadiah digital, kado online, dan voucher secara cepat dan praktis.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${plusJakartaSans.variable} h-full antialiased`}
+    >
+      <body className={`min-h-full flex flex-col font-jakarta-sans`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen w-full">
+      <h1 className="text-3xl font-semibold">GiftKuy.id</h1>
+    </main>
+  );
+}
