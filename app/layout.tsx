c6a74c2deeb809709528acd4eeb56eb03e615d56
bagoesrex <bagoesrex@gmail.com>
2026-05-13 04:13:22 +0700

    feat: add initial header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/header";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       className={`${plusJakartaSans.variable} h-full antialiased`}
     >
       <body className={`min-h-full flex flex-col font-jakarta-sans`}>
+        <Header />
         {children}
       </body>
     </html>
